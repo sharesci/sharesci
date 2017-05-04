3b5f4733f92c3495cf047c110d2f1eaacc22818a
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-05-04 02:29:57 +0000

    Improve server startup script

--- script/start_server.sh
+++ script/start_server.sh
@@ -2,14 +2,18 @@
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "$SOURCE_DIR"
+cd "$SOURCE_DIR/.."
 
-SERVER_PID="$(lsof -t -i:7080 -sTCP:LISTEN)"
+SERVER_PID="$(lsof -t -i:80 -sTCP:LISTEN)"
+kill "$SERVER_PID"
+SERVER_PID="$(lsof -t -i:443 -sTCP:LISTEN)"
 kill "$SERVER_PID"
 
 npm install
 cd clients
 npm install
 npm run build:nowatch
+cd ..
+psql -d sharesci < script/pg_db_schema_setup.sql
 nodejs "server.js" &
 
