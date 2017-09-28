638b771161721ccf30c4dde6bd92cb6d19e881f5
Mike D'Arcy <m.m.darcy@vikes.csuohio.edu>
2017-09-27 20:54:02 -0400

    Add MongoDB to database setup script

--- script/set_up_database.sh
+++ script/set_up_database.sh
@@ -8,6 +8,8 @@ cd "$SOURCE_DIR/.."
 sudo service mongod start
 sudo service postgresql start
 
+mongo sharesci --eval 'db.papers.createIndex({"$**": "text"});'
+
 sudo su -c 'psql <<< "
 	CREATE USER sharesci WITH LOGIN PASSWORD '\''sharesci'\'';
 	CREATE DATABASE sharesci;
@@ -16,6 +18,7 @@ sudo su -c 'psql <<< "
 	CREATE EXTENSION CITEXT;"
 ' postgres
 
+# TODO: This is insecure and should be updated to use a .pgpass file
 sudo PGPASSWORD='sharesci' psql -U sharesci -h localhost -d sharesci < script/pg_db_schema_setup.sql
 
 cd "$original_dir"
