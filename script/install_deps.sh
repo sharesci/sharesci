27dd1e2d1e839dccea26923c15eb538271eb6f19
Mike D'Arcy <mike.darcy@posteo.net>
2018-01-20 00:15:35 -0500

    Update software versions in dependency installer

--- script/install_deps.sh
+++ script/install_deps.sh
@@ -26,8 +26,8 @@ fi
 
 if [[ -z "$(which mongo)" ]] ; then
 	printf "Installing MongoDB\n"
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-	echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 	sudo apt-get update
 	sudo apt-get install -y mongodb-org
 fi
