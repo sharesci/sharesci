63c4c7816e4fcc117d7c5d5770ff058ce58930f7
John Wolf <j.b.wolf68@vikes.csuohio.edu>
2018-01-24 10:36:46 -0500

    New search API

--- controllers/api/v1/search.js
+++ controllers/api/v1/search.js
@@ -1,8 +1,9 @@
 const
 	express = require('express'),
 	assert = require('assert'),
-	MongoClient = require('mongodb').MongoClient,
-	mongo_url = 'mongodb://localhost:27017/sharesci';
+	request = require('request');
+	rp = require('request-promise');
+	http = require('http');
 
 function index(req, res) {
 	var responseJSON = {
@@ -12,79 +13,40 @@ function index(req, res) {
 	};
 
 	var searchParams = JSON.parse(JSON.stringify(req.query));
+
 	if(!searchParams.offset) {
 		searchParams.offset = 0;
 	}
 	if(!searchParams.any) {
 		searchParams['any'] = 'estimation';
 	}
+	if(!searchParams.uri) {
+		searchParams['uri'] = 'http://127.0.0.1/api/v1/searchPapers';
+	}
 	if(searchParams.maxResults) {
 		searchParams.maxResults = parseInt(searchParams.maxResults);
 	}
-	var searchPromise = new Promise((resolve, reject)=>{doSearch(searchParams, resolve, reject);});
-	searchPromise.then((results) => {
-		responseJSON.results = results.results;
-		responseJSON.numResults = results.numHits;
-		res.json(responseJSON);
-		res.end();
-	})
-	.catch((err)=>{
-		responseJSON.errno = 1;
-		res.json(responseJSON);
-		res.end();
-	});
-}
-
-function doSearch(params, resolve, reject) {	
-	MongoClient.connect(mongo_url, function(err, db) {
-		if(err !== null) {
-			console.error("Error opening db");
-			reject(err);
-			return;
-		}
-		
-		var cursor = db.collection('papers').find({'$and':[{'$text': {'$search': params.any}}]}, {'_id': 1, 'title': 1, 'authors': 1, 'updated-date': 1, 'versions': 1, score: {'$meta': 'textScore'}})
-		var numHitsPromise = cursor.count();
-		numHitsPromise.then((data)=>{console.log(data);});
-		cursor.sort({'score': {'$meta': 'textScore'}});
-		cursor.skip(parseInt(params.offset));
-		if(params.maxResults) {
-			cursor.limit(params.maxResults);
-		}
-		Promise.all([cursor.toArray(), numHitsPromise])
-		.then((promiseVals)=>{
-			var arr = promiseVals[0];
-			var numHits = promiseVals[1];
-			if(err){
-				reject(err);
-			} else {
-				resolve({'results': stripVersions(arr), 'numHits': numHits});
-			}
-			db.close();
+	var options = {
+		uri: searchParams.uri,
+		qs: {
+			'offset': searchParams.offset,
+			'any': searchParams.any,
+			'maxResults': searchParams.maxResults
+		},
+		json: true
+	};
+	rp(options)
+		.then(function(results) {
+			responseJSON.results = results.results;
+			responseJSON.numResults = results.numHits;
+			res.json(responseJSON);
+			res.end();
 		})
-		.catch((err)=>{console.error(err);db.close();});
-	});
-}
-
-// Processes the JSON a search results set so any articles with multiple
-// versions show the most recent version
-function stripVersions(resultSet) {
-	for (let i = 0; i < resultSet.length; i++) {
-		if (!('versions' in resultSet[i])) {
-			continue;
-		}
-		// Just take the most recent version and discard everything
-		// else
-		// TODO: Currently this will return extra information like
-		// fulltext and abstract if the version JSON has it; to be
-		// correct, it should only return the same fields specified by
-		// the MongoDB query
-		recentVersion = resultSet[i].versions.slice(-1)[0];
-		recentVersion._id = resultSet[i]._id;
-		recentVersion.score = resultSet[i].score;
-		resultSet[i] = recentVersion;
-	}
-	return resultSet;
+		.catch(function(err) {
+			responseJSON.errno = 1;
+			res.json(responseJSON);
+			res.end();
+		});
 }
 
 module.exports = {
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,7 +3,11 @@ const
 	bodyParser = require('body-parser'),
 	multer = require('multer')({dest:'./uploads'}),
 	searchController = require.main.require('./controllers/api/v1/search'),
+	searchPapersController = require.main.require('./controllers/api/v1/searchPapers');
 	articleController = require.main.require('./controllers/api/v1/articles'),
+	searchWikiController = require.main.require('./controllers/api/v1/searchWiki'),
+	wikiController = require.main.require('./controllers/api/v1/wiki'),
+	commentsController = require.main.require('./controllers/api/v1/comments'),
 	authRouter = require('./auth'),
 	userRouter = require('./user'),
 	usersRouter = require('./users');
@@ -15,14 +19,17 @@ router.use('/user', userRouter);
 router.use('/users', usersRouter);
 
 router.get('/search', searchController.index);
+router.get('/searchPapers', searchPapersController.searchPapers);
+router.get('/searcWiki', searchWikiController.searchWiki);
+router.get('/wiki/:id', wikiController.getWiki);
 
 router.get('/articles/:id', articleController.getArticle);
 router.get('/articles/:id/:version(v\\d+)', articleController.getArticle);
 router.post('/articles', multer.array('fulltextfile', 10));
 router.post('/articles', articleController.postArticle);
 
-
-
+router.get('/comments/:id', commentsController.getComments);
+router.post('/comments', commentsController.postComments);
 
 module.exports = router;
 
