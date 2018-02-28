ef2bace824fe65de7f7d79092df40875c6d973aa
John Wolf <j.b.wolf68@vikes.csuohio.edu>
2018-02-27 20:39:12 -0500

    Updates for comments and userRecommendations

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -7,10 +7,12 @@ const
 	articleController = require.main.require('./controllers/api/v1/articles'),
 	searchWikiController = require.main.require('./controllers/api/v1/searchWiki'),
 	wikiController = require.main.require('./controllers/api/v1/wiki'),
+	commentsController = require.main.require('./controllers/api/v1/comments'),
 	relatedDocsController = require.main.require('./controllers/api/v1/relatedDocs'),
 	relatedDocumentsController = require.main.require('./controllers/api/v1/related-docs'),
 	notifyNewDocsController = require.main.require('./controllers/api/v1/notifyNewDocs'),
 	userHistoryController = require.main.require('./controllers/api/v1/userHistory'),
+	userRecommendationsController = require.main.require('./controllers/api/v1/userRecommendations'),
 	authRouter = require('./auth'),
 	userRouter = require('./user'),
 	usersRouter = require('./users');
@@ -28,16 +30,18 @@ router.get('/wiki/:id', wikiController.getWiki);
 
 router.get('/articles/:id', articleController.getArticle);
 router.get('/articles/:id/:version(v\\d+)', articleController.getArticle);
+router.get('/articles/:id/comments', commentsController.getComments);
 router.post('/articles', multer.array('fulltextfile', 10));
 router.post('/articles', articleController.postArticle);
+router.post('/articles/:id/comments', bodyParser.urlencoded({ extended: true }), commentsController.postComments);
 
 router.post('/notifyNewDocs', notifyNewDocsController.newDoc);
 
 router.get('/relatedDocs', relatedDocsController.relatedDocs);
 router.get('/related-docs', relatedDocumentsController.relatedDocuments);
 
-//router.use('/userHistory', bodyParser.json());
-//router.use('/userHistory', bodyParser.urlencoded({ extended: true }));
 router.post('/userHistory', bodyParser.json(), bodyParser.urlencoded({ extended: true }), userHistoryController.userHistory);
+
+router.get('/userRecommendations', userRecommendationsController.userRecommendations);
 
 module.exports = router;
