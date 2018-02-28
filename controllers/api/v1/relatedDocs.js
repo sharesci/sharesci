9246f81dead44f39fd88c2302f6873a20863b196
John Wolf <j.b.wolf68@vikes.csuohio.edu>
2018-02-27 19:50:36 -0500

    Updated to get full document info

--- controllers/api/v1/relatedDocs.js
+++ controllers/api/v1/relatedDocs.js
@@ -29,13 +29,16 @@ function relatedDocs(req, res) {
 		searchParams.maxResults = parseInt(searchParams.maxResults);
 	}
 	if(!searchParams.uri) {
-		searchParams['uri'] = 'http://127.0.0.1/api/v1/related-docs';
+		searchParams['uri'] = 'http://137.148.142.215:8000/related-docs';
 	}
 	if(!searchParams.collection) {
 		searchParams['collection'] = 'papers';
 	}
 	if(!searchParams.engine) {
 		searchParams['engine'] = 'mongo';
+	}
+	if(!searchParams.getFullDocs) {
+		searchParams['getFullDocs'] = true;
 	}
 	
 	var options = {
@@ -45,17 +48,32 @@ function relatedDocs(req, res) {
 			'docid': searchParams.docid,
 			'maxResults': searchParams.maxResults,
 			'collection': searchParams.collection,
-			'engine': searchParams.engine
+			'engine': searchParams.engine,
+			'getFullDocs': searchParams.getFullDocs
 		},
 		json: true
 	};
 
 	rp(options)
-	.then((results) => {
-		responseJSON.results = results.results;
-		responseJSON.numResults = results.numHits;
-		res.json(responseJSON);
-		res.end();
+	.then((searchResults) => {
+		responseJSON.numResults = searchResults.numHits;
+
+		if(searchParams.getFullDocs) {
+			searchResults['options'] = options;
+			var newPromise = new Promise((resolve, reject) => {getInfo(searchResults, resolve, reject)});
+			newPromise.then((results) => {
+				responseJSON.results = results.results;
+				res.json(responseJSON);
+				res.end();
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+		} else {
+			responseJSON.results = searchResults.results;
+			res.json(responseJSON);
+			res.end();
+		}
 	})
 	.catch(function(err) {
 		responseJSON.errno = 1;
@@ -63,6 +81,82 @@ function relatedDocs(req, res) {
 		res.json(responseJSON);
 		res.end();
 	});
+}
+
+function getInfo(params, resolve, reject) {
+	MongoClient.connect(mongo_url, function(err, db) {
+		if(err !== null) {
+			console.error("Error opening database");
+			reject(err);
+			return;
+		}
+
+		var idObject = params.results.map(obj => { return ObjectId(obj._id) });
+		var newObj = params.results.map(obj => {
+			var nObj = {};
+			nObj[obj._id] = obj.score;
+			return nObj;
+		});
+		var oneObject = Object.assign({}, ...newObj);
+
+		var cursor = db.collection('papers').find({'_id': {$in: idObject}});
+		var numHitsPromise = cursor.count();
+		numHitsPromise.then((data)=>{console.log(data);});
+		cursor.sort({'score': 1});
+		cursor.skip(parseInt(params.options.qs.offset));
+		if(params.options.qs.maxResults) {
+			cursor.limit(parseInt(params.options.qs.maxResults));
+		}
+		Promise.all([cursor.toArray(), numHitsPromise])
+		.then((promiseVals)=>{
+			var arr = promiseVals[0];
+			var numHits = promiseVals[1];
+			var newArr = stripVersions(arr);
+
+			var finalObj = newArr.map(obj => {
+				var fObj = {};
+				fObj['_id'] = obj._id;
+				fObj['documentJson'] = obj;
+				fObj['score'] = oneObject[obj._id];
+				delete fObj.documentJson._id;
+				return fObj;
+			});
+			
+			if(err){
+				reject(err);
+
+			} else {
+				resolve({'results': finalObj, 'numHits': numHits});
+			}
+			db.close();
+		})
+		.catch((err) => {
+			console.error(err);
+			db.close();
+		});
+	});
+}
+
+
+// Processes the JSON a search results set so any articles with multiple
+// versions show the most recent version
+function stripVersions(resultSet) {
+	for (let i = 0; i < resultSet.length; i++) {
+		if (!('versions' in resultSet[i])) {
+			continue;
+		}
+		// Just take the most recent version and discard everything
+		// else
+		// TODO: Currently this will return extra information like
+		// fulltext and abstract if the version JSON has it; to be
+		// correct, it should only return the same fields specified by
+		// the MongoDB query
+		recentVersion = resultSet[i].versions.slice(-1)[0];
+		recentVersion._id = resultSet[i]._id;
+		recentVersion.score = resultSet[i].score;
+		resultSet[i] = recentVersion;
+	}
+	return resultSet;
 }
 
 module.exports = {
--- controllers/api/v1/search.js
+++ controllers/api/v1/search.js
@@ -4,6 +4,9 @@ const
 	session = require('express-session'),
 	request = require('request'),
 	rp = require('request-promise'),
+	ObjectId = require('mongodb').ObjectId,
+	MongoClient = require('mongodb').MongoClient,
+	mongo_url = 'mongodb://localhost:27017/sharesci',
 	http = require('http');
 
 function index(req, res) {
@@ -12,9 +15,7 @@ function index(req, res) {
 		errstr: '',
 		results: []
 	};
-
 	var searchParams = JSON.parse(JSON.stringify(req.query));
-
 	if(!searchParams.offset) {
 		searchParams.offset = 0;
 	}
@@ -25,7 +26,7 @@ function index(req, res) {
 		searchParams.maxResults = parseInt(searchParams.maxResults);
 	}
 	if(!searchParams.uri) {
-		searchParams['uri'] = 'http://127.0.0.1/api/v1/searchPapers';
+		searchParams['uri'] = 'http://137.148.142.215:8000/search';
 	}
 	if(!searchParams.collection) {
 		searchParams['collection'] = 'papers';
@@ -33,39 +34,59 @@ function index(req, res) {
 	if(!searchParams.engine) {
 		searchParams['engine'] = 'mongo';
 	}
-	
+	if(!searchParams.getFullDocs) {
+		searchParams['getFullDocs'] = true;
+	}
+
 	var options = {
 		uri: searchParams.uri,
 		qs: {
 			'offset': searchParams.offset,
-			'any': searchParams.any,
+			'q': searchParams.any,
 			'maxResults': searchParams.maxResults,
 			'collection': searchParams.collection,
-			'engine': searchParams.engine
+			'engine': searchParams.engine,
+			'getFullDocs': searchParams.getFullDocs
 		},
 		json: true
 	};
-
+	
 	rp(options)
-	.then((results) => {
-		responseJSON.results = results.results;
-		responseJSON.numResults = results.numHits;
-		res.json(responseJSON);
-		if(req.session.user_id) {
-			var userSearchOptions = {
-				method: 'POST',
-				uri: 'http://127.0.0.1/api/v1/userHistory',
-				body: {
-					'type': 'terms',
-					'value': searchParams.any,
-					'userid': req.session.user_id
-				},
-				json: true
-			};
-			rp(userSearchOptions).catch(function(err) { console.error('userHistory term post request unsuccessful')});
+	.then((searchResults) => {
+		responseJSON.numResults = searchResults.numHits;
+		
+		if(searchParams.getFullDocs) {
+			searchResults['options'] = options;
+			var newPromise = new Promise((resolve, reject) => {getInfo(searchResults, resolve, reject)});
+			newPromise.then((results) => {
+				responseJSON.results = results.results;
+				res.json(responseJSON);
+				res.end();
+				if(req.session.user_id) {
+					var userSearchOptions = {
+						method: 'POST',
+						uri: 'http://127.0.0.1/api/v1/userHistory',
+						body: {
+							'type': 'terms',
+							'value': searchParams.any,
+							'userid': req.session.user_id
+						},
+						json: true
+					};
+					rp(userSearchOptions).catch(function(err) { console.error('userHistory term post request unsuccessful')});
+				}
+				
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+		} else {
+			responseJSON.results = searchResults.results;
+			res.json(responseJSON);
+			res.end();
 		}
-		res.end();
-	})
+		console.log('responseJSON: ', responseJSON);
+	})	
 	.catch(function(err) {
 		responseJSON.errno = 1;
 		responseJSON.errstr = err.error;
@@ -74,7 +95,82 @@ function index(req, res) {
 	});
 }
 
+function getInfo(params, resolve, reject) {
+	MongoClient.connect(mongo_url, function(err, db) {
+		if(err !== null) {
+			console.error("Error opening database");
+			reject(err);
+			return;
+		}
+
+		var idObject = params.results.map(obj => { return ObjectId(obj._id) });
+		var newObj = params.results.map(obj => {
+			var nObj = {};
+			nObj[obj._id] = obj.score;
+			return nObj;
+		});
+		var oneObject = Object.assign({}, ...newObj);
+
+		var cursor = db.collection('papers').find({'_id': {$in: idObject}});
+		var numHitsPromise = cursor.count();
+		numHitsPromise.then((data)=>{console.log(data);});
+		cursor.sort({'score': 1});
+		cursor.skip(parseInt(params.options.qs.offset));
+		if(params.options.qs.maxResults) {
+			cursor.limit(parseInt(params.options.qs.maxResults));
+		}
+		Promise.all([cursor.toArray(), numHitsPromise])
+		.then((promiseVals)=>{
+			var arr = promiseVals[0];
+			var numHits = promiseVals[1];
+			var newArr = stripVersions(arr);
+
+			var finalObj = newArr.map(obj => {
+				var fObj = {};
+				fObj['_id'] = obj._id;
+				fObj['documentJson'] = obj;
+				fObj['score'] = oneObject[obj._id];
+				delete fObj.documentJson._id;
+				return fObj;
+			});
+			
+			if(err){
+				reject(err);
+
+			} else {
+				resolve({'results': finalObj, 'numHits': numHits});
+			}
+			db.close();
+		})
+		.catch((err) => {
+			console.error(err);
+			db.close();
+		});
+	});
+}
+
+
+// Processes the JSON a search results set so any articles with multiple
+// versions show the most recent version
+function stripVersions(resultSet) {
+	for (let i = 0; i < resultSet.length; i++) {
+		if (!('versions' in resultSet[i])) {
+			continue;
+		}
+		// Just take the most recent version and discard everything
+		// else
+		// TODO: Currently this will return extra information like
+		// fulltext and abstract if the version JSON has it; to be
+		// correct, it should only return the same fields specified by
+		// the MongoDB query
+		recentVersion = resultSet[i].versions.slice(-1)[0];
+		recentVersion._id = resultSet[i]._id;
+		recentVersion.score = resultSet[i].score;
+		resultSet[i] = recentVersion;
+	}
+	return resultSet;
+}
+
 module.exports = {
 	index: index
 };
-
