5654dd793793bccffaa7c9a411e621dd5714f1df
John Wolf <j.b.wolf68@vikes.csuohio.edu>
2018-04-18 11:59:51 -0400

    Various updates to results, fix pager issues

--- client/src/app/components/search-result/search-result.component.ts
+++ client/src/app/components/search-result/search-result.component.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ISearchResults } from '../../models/datacontracts/search-results.interface';
 import { IWSearchResults } from '../../models/datacontracts/wiki-results.interface';
+import { IRelatedDocs } from '../../models/datacontracts/related-docs.interface';
 import { SearchService } from '../../services/search.service';
+import { RelatedDocService } from '../../services/related-doc.service';
 import { PagerService } from '../../services/pager.service';
 import { ArticleService } from '../../services/article.service';
 import { SharedService } from '../../services/shared.service';
@@ -20,16 +22,20 @@ import { saveAs } from 'file-saver'
 export class SearchResultComponent implements OnInit {
     search_results: ISearchResults = null;
     wiki_search_results: IWSearchResults = null;
+    related_docs_results: IRelatedDocs = null;
     search_token = '';
+    docid = '';
     pager: any = {};
     resultPerPage = 10;
     searchType = 'word2vec';
+    articleUrl = 'http://sharesci.org/#/article/';
 
     constructor(private _pagerService: PagerService,
                 private _searchService: SearchService,
                 private _articleService: ArticleService,
                 private _sharedService: SharedService, 
                 private _route: ActivatedRoute,
+                private _relatedDocService: RelatedDocService
                 ) {
 
         _sharedService.searchType$
@@ -43,7 +49,7 @@ export class SearchResultComponent implements OnInit {
                 this._searchService.search(this.search_token, this.searchType)
                     .map(response => <ISearchResults>response)
                     .subscribe(
-                        results => { this.showResults(results); this.setPage(1) },
+                        results => { this.showResults(results); this.setPage(1); },
                         error => console.log(error)
                     );
                 this._searchService.wikiSearch(this.search_token, this.searchType)
@@ -63,15 +69,27 @@ export class SearchResultComponent implements OnInit {
                 results => { this.showResults(results); this.setPage(1);},
                 error => console.log(error)
             );
-            this._searchService.wikiSearch(this.search_token, this.searchType)
+        this._searchService.wikiSearch(this.search_token, this.searchType)
             .map (response => <IWSearchResults>response)
             .subscribe (
                 wikiresults => { this.wikiResults(wikiresults);},
                 error => console.log(error)
             );
     }
-
+    
+    private getRelatedDocs(docid: string, offset: 0, maxResults: 10) {
+        this._relatedDocService.getRelatedDocs(this.docid, offset, maxResults)
+            .map(response => <IRelatedDocs>response)
+            .subscribe(
+            	results => { this.showResults(results); },
+            	error => console.log(error)
+            );
+    }
+    
     private showResults(search_results: ISearchResults) {
+        search_results.results.forEach(obj => {
+            obj['url'] = this.articleUrl + obj._id;
+        });
         this.search_results = search_results;
     }
 
@@ -84,27 +102,27 @@ export class SearchResultComponent implements OnInit {
         let maxResults = 0;
 
         if (this.resultPerPage * page > this.search_results.numResults) {
-            maxResults = this.search_results.numResults % this.resultPerPage
+            maxResults = this.search_results.numResults % this.resultPerPage;
         }
         else {
-            maxResults = 10
+            maxResults = 10;
         }
-        this.search(page, maxResults);
+        this.search(this.pager.startIndex, maxResults);
     }
 
     private setPage(page: number) {
         if (page < 1 || page > this.pager.totalPages) {
             return;
         }
-        this.pager = this._pagerService.getPager(this.search_results.numResults, page);
+        this.pager = this._pagerService.getPager(this.search_results.numResults, page, this.resultPerPage);
     }
 
     private search(offset: number, maxResults: number) {
         this._searchService.search(this.search_token, this.searchType, offset, maxResults)
             .map(response => <ISearchResults>response)
             .subscribe(
-            results => { this.showResults(results); },
-            error => console.log(error)
+            	results => { this.showResults(results); },
+            	error => console.log(error)
             );
 
         this._searchService.wikiSearch(this.search_token, this.searchType, offset, maxResults)
